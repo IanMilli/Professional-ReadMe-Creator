75696df4e3cf971d3351a0277e7454220883d602
Ian M <ianmillichamp.2@gmail.com>
2023-02-17 15:28:37 +0000

    added first questions to array

--- index.js
+++ index.js
@@ -25,8 +25,70 @@ License = license used for the application
 
 /**create an array of questions for the user to respond to */
 const questions = [
+    {
+      type: "input",
+      message: "What is your project's title?",
+      name: "title"
+    },{
+    type: "contents",
+    message: "What Sections do you require for your readme?",
+    name: "contents",
+    choices: ["Description","Technologies","Links","Installation","WireFraming","Development","Usage","Screenshots","Tests","Evolution","Credits","References","Collaboration","Licence"]
+    },{
 
-];
+      type: "input",
+      message: "What is your project's description?",
+      name: "description"
+    },
+    {
+      type: "input",
+      message: "What are your project's installation instructions?",
+      name: "install",
+      default: "npm i"
+    },
+    {
+      type: "input",
+      message: "What is your project's intended usage?",
+      name: "usage"
+    },
+    {
+      type: "input",
+      message: "What technologies did your project use?",
+      name: "tech"
+    },
+    {
+      type: "input",
+      message: "What commands should be run to run tests?",
+      name: "tests",
+      default: "npm run test"
+    },
+    {
+      type: "input",
+      message: "Who needs credit on this project?",
+      name: "credits"
+    },
+    {
+      type: "input",
+      message: "How does someone contribute to this project?",
+      name: "contribute"
+    },
+    {
+      type: "input",
+      message: "What is your email?",
+      name: "email"
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "github"
+    },
+    {
+      type: "list",
+      message: "How is this project licensed?",
+      name: "license",
+      choices: ["Apache", "Boost Software", "Creative Commons", "GNU", "ISC", "MIT", "Mozilla Public", "The Unlicense", "Other"]
+    }
+  ]
 
 // function to write README file
 function writeToFile(fileName, data) {
