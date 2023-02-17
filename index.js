57f31dc3b8b4c52f42dcf9369d49c610133fd992
Ian M <ianmillichamp.2@gmail.com>
2023-02-17 15:20:41 +0000

    pseudo coded sections required for professional readme

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+/**variables provided in the starter code to allow for the creation of the readme file */
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
+/**sections required to be created for a Professional ReadMe File 
+Title = name of application
+Contents = content list to allow user to quickly navigate readme file
+Description = description of application
+Technologies = technologies used to create application
+Links = links to repository for application
+Installation = how to install the application
+WireFraming = section to include any wireframing carried out for application
+Development = how the application was developed
+Usage = how to use the application
+Screenshots = screenshots of the application
+Tests = evidence of tests carried out to ensure the functuality of the application
+Evolution = how the application may be evolved further
+Credits =  authors of the application and contact details
+References = list of reference materials used in the creation of the application
+Collaboration = if required the collaboration agreement
+License = license used for the application
+*/
+
+/**create an array of questions for the user to respond to */
 const questions = [
 
 ];
